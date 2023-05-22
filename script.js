1285724ab4acbb5fde130ea40a397eceb6fec20e
ethan7lesar <ethanlesar11@gmail.com>
2023-05-22 16:38:33 +0200

    V3 Update

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /*Intializing a variable called display which takes in from the html input with the class of screen */
 
 let display = document.getElementById('screen'); 
+let operator = document.getElementsByClassName('.operator')
 
  function clearSum(){
   display.value = '';
@@ -8,15 +9,63 @@ let display = document.getElementById('screen');
  }
 
   function input(i){
-      display.value += i
+    
+    
+    // if (!operator) {
+    //     display.value += i;
+    //     operator = true;
+    //   } else if (i !== '+' && i !== '-' && i !== '*' && i !== '/') {
+    //     display.value += i;
+    //     operator = false;
+    //   }
+    // if(i != '.'){
+        
+        // display.value = display.length - 1;
+        display.value += i
+        
+    //  } else if(i = '.')   {
+        
+         
+    //    display.value = display.value.pop(-1)
+    // }// else{
+        // display.value += i;
+    //  }
 
- } 
+  }
 
   function evaluateSum(){
 
       display.value = eval(display.value)
 
   }
+
+  function deleteInput(){
+    display.value = display.value.slice(0, -1)
+  }
+
+//   function calc(x){
+// display.value = x
+// ;
+//   }
+//    function input(num) {
+//     
+//      let screenArray = display.split('')
+//     
+//      if(num != '.'){
+//     
+//          screenArray.push(num)
+//          return display.value = screenArray.join('');
+//      }
+//      else if(num === '.' && screenArray.includes('.')){
+//         
+//          return display.value = screenArray.join('');
+//      } else {
+//          screenArray.push('.')
+//          
+
+//          display.value= screenArray.join('');
+//      }
+//  }
     
 
   
